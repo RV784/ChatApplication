4c32a38b3eb43f75ce29fcefea626f6de0cee809
Rajat verma <rajatverma@Rajats-MacBook-Pro.local>
2023-09-28 18:56:09 +0530

    Created project structure

---
+++ Massenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Massenger
+//
+//  Created by Rajat verma on 28/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
